4e6d2267dfd50b2b65990481a73b09a14477cc18
Nuvallo <nuvallo.inc@gmail.com>
2019-10-10 16:05:31 -0400

    starting conversion

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,36 @@
 // CODE here for your Lambda Classes
+class GameObject {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.createdAt = attributes.createdAt;
+    this.dimensions = attributes.dimensions;
+  }
+
+  GameObject() {
+    return `${this.name} was removed from the game.`;
+  }
+}
+
+class CharacterStats extends GameObject {
+  constructor(characterAttributes) {
+    super(characterAttributes);
+    this.name = characterAttributes.name;
+    this.healthPoints = characterAttributes.healthPoints;
+  }
+
+  CharacterStats() {
+    return `${this.name} took damage`;
+  }
+}
+
+class Humanoid extends CharacterStats {
+  constructor(humanoidAttributes) {
+    super(characterAttributes);
+    this.name = characterAttributes.name;
+    this.healthPoints = characterAttributes.healthPoints;
+  }
+
+  CharacterStats() {
+    return `${this.name} took damage`;
+  }
+}
